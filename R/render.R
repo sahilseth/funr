30501da5739d65f60ebb566a6d6a8da0b95fe6e9
sahil seth <sahiilseth@gmail.com>
2021-02-09 11:27:27 -0600

    use `saveRDS` instead of write_rds

--- R/render.R
+++ R/render.R
@@ -35,7 +35,7 @@ render_funr <- function(x, max_rows = 100){
     # saved in the cwd as funr_results.rds
   }else if(is.list(out) & (object.size(out)/1024) > 200){
     message("This seems like a large list object. Saving results as rds file funr_results.rds")
-    write_rds(out, "funr_results.rds")
+    saveRDS(out, "funr_results.rds")
     # try(head(out, max_rows), silent = TRUE)
 
   }else if(is.data.frame(out)){
